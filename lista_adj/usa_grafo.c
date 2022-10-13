73df2dafbab6fe50e4470da8ffea1cb7ca25791b
Daniele Karoline <carvalho.danielekr@gmail.com>
2022-10-13 11:44:21 -0300

    inserindo

--- lista_adj/usa_grafo.c
+++ lista_adj/usa_grafo.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "aresta.h"
 #include "aresta.c"
-#include "graph.h"
 #include "graph.c"
 
 int main() {
-    Grafo *grafo;
+    Grafo *g;
     
-    grafo = GRAFOconstroi(4);
-
-    GRAFOinsere_aresta(grafo, ARESTA(0,2));
-    GRAFOinsere_aresta(grafo, ARESTA(0,1));
-    GRAFOinsere_aresta(grafo, ARESTA(3,1));
-    GRAFOinsere_aresta(grafo, ARESTA(2,0));
-    GRAFOinsere_aresta(grafo, ARESTA(2,1));
-    GRAFOimprime(grafo);
+    g = GRAFOconstroi(4);
+    GRAFOinsere_aresta(g, ARESTA(0,1));
+    GRAFOinsere_aresta(g, ARESTA(2,3));
+    GRAFOinsere_aresta(g, ARESTA(3,1));
+    GRAFOinsere_aresta(g, ARESTA(3,3));
+    GRAFOimprime(g);
+    //GRAFOremove_aresta(g, ARESTA(2,3))
+    GRAFOimprime(g);
+    
+    
 
     return 0;
 }
