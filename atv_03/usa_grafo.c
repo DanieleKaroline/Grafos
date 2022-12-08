50aaff03e28126e840fea3746beaf2f3c81a9669
Daniele Karoline <carvalho.danielekr@gmail.com>
2022-12-08 07:12:37 -0300

    testes

--- atv_03/usa_grafo.c
+++ atv_03/usa_grafo.c
@@ -1,50 +1,32 @@
-/*
- * Tarefa 02 - Distancias e Caminhos
- *
- * GEN254 - Grafos - 2022/2
- *
- * Nome:      XXXX
- * Matricula: XXXX
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "aresta.c"
 #include "grafo.c"
 
 int main() {
-    int V;
-    int E;
     Grafo *grafo;
-    int v1;
-    int v2;
-    int C;
-    int p;
+    int v1, v2, v, e, p;
+    
 
-    scanf("%d %d", &V, &E);
+    scanf("%d %d", &v, &e);
 
-    grafo = GRAFOconstroi(V);
+    grafo = GRAFOconstroi(v);
 
-    for (int i = 0; i < E; i++) {
+    for (int i = 0; i < e; i++) {
         scanf("%d %d %d", &v1, &v2, &p);
         GRAFOinsere_aresta(grafo, ARESTA(v1, v2, p));
-        GRAFOimprime(grafo);
+        
     }
+    scanf("%d %d", &v1, &v2);
+    GRAFOremove_aresta(grafo, ARESTA(v1, v2, 0));
     GRAFOimprime(grafo);
-    /*scanf("%d", &C);
-
-    if (C > 0) {
-        for (int i = 0; i < (C - 1); i++) {
-            scanf("%d", &v1);
-            GRAFOimprime_distancias_caminhos(grafo, v1);
-
-            printf("\n");
-        }
-
-        scanf("%d", &v1);
-        GRAFOimprime_distancias_caminhos(grafo, v1);
-    }*/
-
+   /*
+   scanf("%d", &c);
+    for(int i = 0; i < c; i++){
+        scanf("%d %d", &v1, &v2);
+        GRAFOdij(grafo, ARESTA(v1, v2));
+    }
+   */ 
     GRAFOdestroi(grafo);
 
     return 0;
